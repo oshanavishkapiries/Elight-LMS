159fadb02c6e41a3210e2fe80c4bc39f710cd271
OSHAN AVISHKA <153918928+oshanavishkapiries@users.noreply.github.com>
2024-07-31 09:41:39 +0530

    conditional-render-applied

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,27 +1,28 @@
+"use client";
 import Nav from "@/components/main/Nav";
 import { MobileBottomNav } from "@/components/main/MobileBottomNav";
 import MobileNav from "@/components/main/MobileNav";
 import Sider from "@/components/main/Sider";
+import useWindowSize from "@/hooks/useWindowSize";
 
 const layout = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const size = useWindowSize();
+  const isMobile = size.width && size.width <= 768;
+
   return (
     <>
       <main className="grid min-h-screen w-full md:grid-cols-[240px_1fr] overflow-hidden">
-        {/* sider */}
-        <Sider />
-        {/* dashboard-pages */}
+        {isMobile ? <></> : <Sider />}
+
         <div className="flex flex-col">
-          {/* navbar */}
-          <Nav />
-          {/* mobile-nav */}
-          <MobileNav />
+          {isMobile ? <MobileNav /> : <Nav />}
+
           <main className="flex-1 overflow-auto">{children}</main>
-          {/*mobile-bottom-nav */}
-          <MobileBottomNav />
+          {isMobile ? <MobileBottomNav /> : <></>}
         </div>
       </main>
     </>
